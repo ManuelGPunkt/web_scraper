9a7d46b0b860a83575dc1236713ee1d39f15fa32
Mark Becher <mark.leon.becher@gmail.com>
2023-08-26 18:03:04 +0200

    seperating checkPWD and checkInput into smaller chunks

--- web_interface/main.js
+++ web_interface/main.js
@@ -7,4 +7,9 @@ function init(e)
     let sForm = document.getElementsByName('sForm')[0];
     let url = document.getElementsByName('url')[0];
     let pwd = document.getElementsByName('pwdCheck')[0];
+
+    sForm.addEventListener('submit', (e) => //formular bekommt den Wert false, wenn die Eingabe nicht stimmt.
+    {
+        Behavior().checkInput(url);
+    });
 }
--- web_interface/scraper.js
+++ web_interface/scraper.js
@@ -4,48 +4,50 @@ class Behavior
 {
     constructor()
     {   
-        this.connect = new CreateConnection();
-        document.addEventListener('DOMContentLoaded', init)
+        this.connect = new CreateConnection(url);
     }
 
-    init(e)
-    {
         /**
         let sForm = document.getElementsByName('sForm')[0];
         let url = document.getElementsByName('url')[0];
         let pwd = document.getElementsByName('pwdCheck')[0];
         */
 
-        checkPWD(pwd)
-        {
+    async checkPWD(pwd)
+    {
             /**
                 es soll ein passwort eingeben werden und anschliessend ueberprueft werden.
                 wenn es korrekt ist, soll zugelassen werden, dass das Programm die Website der gegebenen URL scraped.
             */
-           if(sha256(pwd.value) == "f395c41bd385de8883ffbe089efa60d9fa809ef626135a9dd848bf5b10d5b793")
-           {
-            this.connect(url.value);
-            return true; //wenn das Passwort stimmt, wird die Nachricht an den Server geschickt, ansonsten nicht.
-           }
-           false;
-        }
-
-        checkInput(url) //es muss fuer diese funktion der Wert uebergeben werden
-        {
-            if(url != "" && !(url.includes("'") || url.includes("\"")) && checkPWD(pwd))
-            {
-                checkPWD(pwd)
-            }
-        }
-
+        const hashedPW = sha256(pwd.value);
+        return hashedPW === "f395c41bd385de8883ffbe089efa60d9fa809ef626135a9dd848bf5b10d5b793"; // gibt true oder false zurueck
+        
     }
+
+    async checkInput(url) //es muss fuer diese funktion der Wert uebergeben werden
+    {
+        url = url.value;
+
+        if(url == "" || (url.includes("'") || url.includes("\"")))
+        {
+            alert("URL contains invalid characters!");
+            return false; //das html formular bekommt den Wert false
+        }
+
+        const pwdCorrect = await this.checkPWD(pwd); //wenn das Passwort korrekt ist, wird eine Verbindung aufgebaut
+        if(pwdCorrect)
+        {
+            await this.connect(url);
+        }
+    }
+
 }
 
 class CreateConnection  //Diese Klasse stellt eine Verbindung zum python Web scraper her, dieser ist mit der Datenbank verbunden.
 {
     constructor(url)
     {
-        this.url = url
+        this.url.value = url
         this.socket = new WebSocket("ws://localhost:49153");
 
         this.socket.onopen = () =>
@@ -54,8 +56,10 @@ class CreateConnection  //Diese Klasse stellt eine Verbindung zum python Web scr
         };
 
         this.socket.addEventListener('error', (e) =>
+        {
+            alert("connection to server could not be astablished!");
             console.log("Server unreachable")
-        );
+        });
 
         this.socket.onmessage = (event) =>
         {
