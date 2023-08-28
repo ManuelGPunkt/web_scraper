53d18b3e1e7091c0cf8895d7ee61a5503dc5e23e
Mark Becher <mark.leon.becher@gmail.com>
2023-08-28 20:06:21 +0200

    first implementation of the database interface

--- database_connection.py
+++ database_connection.py
@@ -1,3 +1,18 @@
-'''
-dieses Modul soll die Schnittstelle zu einer relationalen Datenbank sein
-'''
+
+class DBconnect:
+    '''
+    dieses Modul soll die Schnittstelle zu einer relationalen Datenbank sein
+    '''
+    
+    def __init__(self, url : str, *parameters : str) -> None:
+        self.url = url  #query soll die URL der Website sein
+        self.parameters = parameters  #queryResult sollen die Daten von der SQL-Abfrage sein
+
+    def checkEntry(url) -> bool: #es soll ueberprueft werden, ob es fuer die URL bereits einen Eintrag gibt
+        pass
+
+    def buildQuery(url : str, parameters : str):
+        pass
+
+    def returnQueryResult() -> None:    #es sollen die Daten in JSON formatiert an den Client zurueck geschickt werden
+        pass
