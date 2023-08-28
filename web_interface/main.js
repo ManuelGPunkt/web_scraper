faf3dfe3e025a50a52f3db0ee892db8d7cb137ab
Mark Becher <mark.leon.becher@gmail.com>
2023-08-28 21:56:16 +0200

    changes to client side, improvement to behavior, connection and implementation of the cryptography library in the HTML file

--- web_interface/main.js
+++ web_interface/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', init);
-
-function init(e)
-{
+document.addEventListener('DOMContentLoaded', function init(e) {
+    
     let sForm = document.getElementsByName('sForm')[0];
-    let url = document.getElementsByName('url')[0];
-    let pwd = document.getElementsByName('pwdCheck')[0];
 
     sForm.addEventListener('submit', (e) => //formular bekommt den Wert false, wenn die Eingabe nicht stimmt.
     {
-        Behavior().checkInput(url);
+        e.preventDefault();
+
+        const inputCheck = new Behavior();
+        inputCheck.checkInput();
     });
-}
+
+});
--- web_interface/scraper.js
+++ web_interface/scraper.js
@@ -4,14 +4,12 @@ class Behavior
 {
     constructor()
     {   
-        this.connect = new CreateConnection(url);
-    }
+        this.connect = new CreateConnection();
 
-        /**
-        let sForm = document.getElementsByName('sForm')[0];
-        let url = document.getElementsByName('url')[0];
-        let pwd = document.getElementsByName('pwdCheck')[0];
-        */
+        this.sForm = document.getElementsByName('sForm')[0];
+        this.url = document.getElementsByName('url')[0];
+        this.pwd = document.getElementsByName('pwdCheck')[0];
+    }
 
     async checkPWD(pwd)
     {
@@ -19,25 +17,25 @@ class Behavior
                 es soll ein passwort eingeben werden und anschliessend ueberprueft werden.
                 wenn es korrekt ist, soll zugelassen werden, dass das Programm die Website der gegebenen URL scraped.
             */
-        const hashedPW = sha256(pwd.value);
+        const hashedPW = CryptoJS.SHA256(pwd.value).toString();
         return hashedPW === "f395c41bd385de8883ffbe089efa60d9fa809ef626135a9dd848bf5b10d5b793"; // gibt true oder false zurueck
         
     }
 
     async checkInput(url) //es muss fuer diese funktion der Wert uebergeben werden
     {
-        url = url.value;
+        let urlValue = this.url.value;
 
-        if(url == "" || (url.includes("'") || url.includes("\"")))
+        if(urlValue == "" || (urlValue.includes("'") || urlValue.includes("\"")))
         {
-            alert("URL contains invalid characters!");
-            return false; //das html formular bekommt den Wert false
+            alert("URL invalid!");
+            return; //der gesammte Ablauf des Programms wird unterbrochen
         }
 
-        const pwdCorrect = await this.checkPWD(pwd); //wenn das Passwort korrekt ist, wird eine Verbindung aufgebaut
+        const pwdCorrect = await this.checkPWD(this.pwd); //wenn das Passwort korrekt ist, wird eine Verbindung aufgebaut
         if(pwdCorrect)
         {
-            await this.connect(url);
+            this.connect().send(urlValue);
         }
     }
 
@@ -45,31 +43,34 @@ class Behavior
 
 class CreateConnection  //Diese Klasse stellt eine Verbindung zum python Web scraper her, dieser ist mit der Datenbank verbunden.
 {
-    constructor(url)
+    constructor()
     {
-        this.url.value = url
+        this.url = document.getElementsByName('url')[0];
         this.socket = new WebSocket("ws://localhost:49153");
 
         this.socket.onopen = () =>
         {
-            this.socket.send(url)    //hier muss die anzugebende URL als parameter drin stehen.
+            console.log("connection established");
+            this.socket.send(url.value);    //hier muss die anzugebende URL als parameter drin stehen.
         };
 
-        this.socket.addEventListener('error', (e) =>
+        this.socket.onerror = () =>
         {
-            alert("connection to server could not be astablished!");
-            console.log("Server unreachable")
-        });
+            //alert("connection to server could not be astablished!");
+            console.log("Server unreachable");
+        };
 
         this.socket.onmessage = (event) =>
         {
-            console.log("received data: ", event.data)   //hier muss als Antwort die gelieferten Daten stehen.
+            console.log("received data: ", event.data);   //hier muss als Antwort die gelieferten Daten stehen.
+            alert("eine Nachricht ist eingegangen");
             //Die Daten sollen auf der Website und nicht in der Konsole stehen.
             document.getElementsByTagName('span')[0].innerHTML = event.data;
         };
 
         this.socket.onclose = () =>
         {
+            //alert("Die Verbindung wurde getrennt");
             console.log("connection closed");
         };
     }
