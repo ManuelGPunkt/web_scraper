e7d5e8f0babb6c1da2293184b1b1649039384f01
Mark Becher <mark.leon.becher@gmail.com>
2023-10-21 18:49:55 +0200

    update to web client, also message for URL and message for parameters are now seperate ones

--- web_interface/main.js
+++ web_interface/main.js
@@ -3,13 +3,40 @@
 document.addEventListener('DOMContentLoaded', function init(e) {
     
     let sForm = document.getElementsByName('sForm')[0];
+    
+    const URL = document.getElementsByName('url')[0];
+    const param = document.getElementsByName('param')[0];
 
     sForm.addEventListener('submit', (e) => //formular bekommt den Wert false, wenn die Eingabe nicht stimmt.
     {
-        e.preventDefault();
+        //e.preventDefault();
 
         const inputCheck = new Behavior();
-        inputCheck.checkInput();
+        
+        if(inputCheck.checkPWD() && inputCheck.checkURL())
+        {
+            if(inputCheck.checkParameters()) //wenn die parameter gesetzt und gueltig sind
+            {
+                const connect = new CreateConnection(URL, param);   //es wird die URL und parameter als argument uebergeben
+            }
+
+            else    //wenn die parameter nicht gesetzt oder ungueltig sind
+            {
+                const connect = new CreateConnection(URL); //es wird die URL als argument uebergeben
+            }
+        }
+
+        else    //wenn die Bedingungen fuer das Formular nicht erfuellt sind.
+        {
+            const elements = document.getElementsByClassName('textfield');
+
+            Array.from(elements).forEach((element) => { //alle Eingabefelder werden der Klasse error zugeordnet und rot gefaerbt.
+                element.classList.add("error");
+            });
+
+
+            return false;   //das Formular wird nicht abgeschickt.
+        }
     });
 
 });
--- web_interface/scraper.js
+++ web_interface/scraper.js
@@ -4,54 +4,70 @@ class Behavior
 {
     constructor()
     {   
-        this.connect = new CreateConnection();
-
         this.sForm = document.getElementsByName('sForm')[0];
         this.url = document.getElementsByName('url')[0];
         this.pwd = document.getElementsByName('pwdCheck')[0];
+        this.param = document.getElementsByName('param')[0];
     }
 
-    async checkPWD(pwd)
+    async checkPWD()
     {
+        let PWD = this.pwd.value;
             /**
                 es soll ein passwort eingeben werden und anschliessend ueberprueft werden.
                 wenn es korrekt ist, soll zugelassen werden, dass das Programm die Website der gegebenen URL scraped.
             */
         const hashedPW = CryptoJS.SHA256(pwd.value).toString();
-        return hashedPW === "f395c41bd385de8883ffbe089efa60d9fa809ef626135a9dd848bf5b10d5b793"; // gibt true oder false zurueck
-        
+        return hashedPW === "f395c41bd385de8883ffbe089efa60d9fa809ef626135a9dd848bf5b10d5b793"; // gibt true oder false zurueck. Das Passwort ist: 8h+H)=(+n-Y%DG78
     }
 
-    async checkInput(url) //es muss fuer diese funktion der Wert uebergeben werden
+    async checkURL() //es wird die gueltigkeit der URL ueberprueft
     {
-        let urlValue = this.url.value;
+        const URL = this.url.value;
 
-        if(urlValue == "" || (urlValue.includes("'") || urlValue.includes("\"")))
+        if(URL == "" || (URL.includes("'") || URL.includes("\"")))
         {
             alert("URL invalid!");
-            return; //der gesammte Ablauf des Programms wird unterbrochen
-        }
-
-        const pwdCorrect = await this.checkPWD(this.pwd); //wenn das Passwort korrekt ist, wird eine Verbindung aufgebaut
-        if(pwdCorrect)
-        {
-            this.connect().send(urlValue);
+            return false; //der gesammte Ablauf des Programms wird unterbrochen
         }
     }
 
+    async checkParameters() //wenn die Parameter gueltig sind, werden sie als einziger string an den web scraper geschickt und dort zerteilt.
+    {
+        const PARAM = this.param.value;
+
+        if(PARAM == "" || (PARAM.includes("'") || PARAM.includes("\""))) //ueberpruefen ob der String ungueltige Zeichen enthaelt, ', " (SQL Injection) oder leer ist.
+        {
+            return false;
+        }
+
+        else
+        {
+            return PARAM; //die einzelnen Parameter sollen dann vom Web Scraper selbst verarbeitet werden
+        }
+    }
 }
 
 class CreateConnection  //Diese Klasse stellt eine Verbindung zum python Web scraper her, dieser ist mit der Datenbank verbunden.
 {
-    constructor()
+    constructor(msg1, msg2)
     {
         this.url = document.getElementsByName('url')[0];
         this.socket = new WebSocket("ws://localhost:49153");
 
-        this.socket.onopen = () =>
+        this.socket.onopen = () => //mit dieser funktion koennen die Nachricht an den Scraper gesendet werden.
         {
             console.log("connection established");
-            this.socket.send(url.value);    //hier muss die anzugebende URL als parameter drin stehen.
+
+            if(msg1)
+            {
+                this.socket.send(msg1);    //hier muss die anzugebende URL als parameter drin stehen.
+            }
+            
+            if(msg2)
+            {
+                this.socket.send(msg2); //hier werden die Parameter als String an den Scraper geschickt.
+            }
         };
 
         this.socket.onerror = () =>
@@ -64,7 +80,8 @@ class CreateConnection  //Diese Klasse stellt eine Verbindung zum python Web scr
         {
             console.log("received data: ", event.data);   //hier muss als Antwort die gelieferten Daten stehen.
             alert("eine Nachricht ist eingegangen");
-            //Die Daten sollen auf der Website und nicht in der Konsole stehen.
+
+            //Die Daten sollen auf der Website angezeigt werden und nicht in der Konsole stehen.
             document.getElementsByTagName('span')[0].innerHTML = event.data;
         };
 
