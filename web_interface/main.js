6b8defdac5bfcdebe3c2c628299286802f59a885
Mark Becher <mark.leon.becher@gmail.com>
2023-11-03 22:32:27 +0100

    change to structure of main

--- web_interface/main.js
+++ web_interface/main.js
@@ -1,27 +1,27 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function init(e) {
-    
-    let sForm = document.getElementsByName('sForm')[0];
-    
+document.addEventListener('DOMContentLoaded', init());
+
+function main()
+{
+    const sForm = document.getElementsByName('sForm')[0];
     const URL = document.getElementsByName('url')[0];
     const param = document.getElementsByName('param')[0];
 
-    sForm.addEventListener('submit', (e) => //formular bekommt den Wert false, wenn die Eingabe nicht stimmt.
-    {
-        //e.preventDefault();
+    const inputCheck = new Behavior();
+    const connect = new CreateConnection(URL, param);
 
-        const inputCheck = new Behavior();
+    sForm.addEventListener('submit', (e) => //formular bekommt den Wert false, wenn die Eingabe nicht stimmt.
+    {   //es gibt kein Formular, muss noch geandert werden.
+        //e.preventDefault();
         
         if(inputCheck.checkPWD() && inputCheck.checkURL())
         {
-            const connect = new CreateConnection(URL, param);   //es wird die URL und parameter als argument uebergeben
+            connect.connect();   //es wird die URL und parameter als argument uebergeben
         }
 
         else    //wenn die Bedingungen fuer das Formular nicht erfuellt sind.
         {
-            const elements = document.getElementsByClassName('textfield');
-
             Array.from(elements).forEach((element) => { //alle Eingabefelder werden der Klasse error zugeordnet und rot gefaerbt.
                 element.classList.add("error");
             });
@@ -30,4 +30,9 @@ document.addEventListener('DOMContentLoaded', function init(e) {
         }
     });
 
-});
+};
+
+function init(e)
+{
+    main();
+}
