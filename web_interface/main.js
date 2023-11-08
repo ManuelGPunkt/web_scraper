108347375d631fb50fabb92f70108acd58bb328e
Mark Becher <mbecher@stud.hs-offenburg.de>
2023-11-08 23:10:03 +0100

    removed form EventListener, because no form element is submitted

--- web_interface/main.js
+++ web_interface/main.js
@@ -11,25 +11,20 @@ function main()
     const inputCheck = new Behavior();
     const connect = new CreateConnection(URL, param);
 
-    sForm.addEventListener('submit', (e) => //formular bekommt den Wert false, wenn die Eingabe nicht stimmt.
-    {   //es gibt kein Formular, muss noch geandert werden.
-        //e.preventDefault();
-        
-        if(inputCheck.checkPWD() && inputCheck.checkURL())
-        {
-            connect.connect();   //es wird die URL und parameter als argument uebergeben
-        }
 
-        else    //wenn die Bedingungen fuer das Formular nicht erfuellt sind.
-        {
-            Array.from(elements).forEach((element) => { //alle Eingabefelder werden der Klasse error zugeordnet und rot gefaerbt.
-                element.classList.add("error");
-            });
+    if(inputCheck.checkPWD() && inputCheck.checkURL())
+    {
+        connect.sendMessage();   //es wird die URL und parameter als argument uebergeben
+    }
 
-            return false;   //das Formular wird nicht abgeschickt.
-        }
-    });
+    else    //wenn die Bedingungen fuer das Formular nicht erfuellt sind.
+    {
+        Array.from(elements).forEach((element) => { //alle Eingabefelder werden der Klasse error zugeordnet und rot gefaerbt.
+        element.classList.add("error");
+        });
 
+        return false;   //das Formular wird nicht abgeschickt.
+    }
 };
 
 function init(e)
