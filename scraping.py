4b87c00b060592ab1ab883ade3b5cb39279c1c26
Mark Becher <mbecher@stud.hs-offenburg.de>
2024-04-26 22:46:34 +0200

    method to strip and select tags from the html code, implemented

--- scraping.py
+++ scraping.py
@@ -1,41 +1,76 @@
 from bs4 import BeautifulSoup
-import csv
 import requests
+import xml.etree.ElementTree as Et
 
 class Scraper:
     def __init__(self, url:str, params:str) -> None:
         self.url:str = url
         self.params = params
+        self.tags_filtered = [] #hier werden die Tags ohne wiederholung gespeichert.
+        self.contentString = "" #hier wird der komplette Inhalt in einem String zwischengespeichert.
 
         response = requests.get(url)
         html = BeautifulSoup(response.text, 'html.parser')
 
-    def scrapeTags() -> None:
-        pass
+    #private Methode
+    def __stripTags(self) -> None:
+        used = set() #damit tags nicht mehrfach gescraped werden
+        tags_unfiltert = self.params.split()
+        
+        #hier werden doppelte tags entfernt
+        for i in tags_unfiltert:
+            if i not in used:
+                used.add(i)
+                self.tags_filtered.append(i)
+
+    
+    def selectTags(self) -> str: 
+        """
+        diese Methode gibt einen String zurueck,
+        damit dieser in der Datenbank gespeichert werden kann.
+        """
+        
+        self.__stripTags()
+    
+        for tags in self.tags_filtered:
+            placeHolder = html.find_all(f"{tags}", class_='')
+            for w in placeHolder:
+                self.contentString += str(w) + "\n"
+
+        return self.contentString
+
+    
+    def format_to_XML(self) -> str:
+        """
+        Diese Methode erstellt eine XML Format aus den Daten,
+        damit diese an der Client zurueck geschickt werden koennen.
+        """
+
+        self.selectTags()
+        
+        return self.contentString
+        
 
 #-----------------------------------------------------------------------------------------------------
 
 # url setzen
 url = "https://quotes.toscrape.com/"
+#url = "file:///home/mark/Desktop/testit"
 
 # GET-request ausfuehren
 response = requests.get(url)
 
+#print(response.text)
+
 #html dokument parsen
-html = BeautifulSoup(response.text, 'html.parser')
+html = BeautifulSoup(response.text, "html.parser")
+
+#print(type(html.text)) #fuer die Datenbank html.text verwenden, datentyp String
 
 #classes von tag filtern
-tag = html.find_all('div', class_="tag")
+tag = html.find_all('a', class_="tag")
 
-#in einer liste sammeln
-tags = list()
-for tags_ in tag:
-    tags.append(tags_.text)
+#print(type(str(tag)))
 
-#Einträge ausgeben
-for i in tags:
-    print(i)
-
-with open('./zitate.csv', 'w', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file, dialect='excel')
-    csv_writer.writerows([[tag] for tag in tags])
+testit = Scraper(url, "a a p br span _")
+print(testit.format_to_XML())
