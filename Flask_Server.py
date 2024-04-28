40c1d745fe25a891412156ccafc597f03245e5ee
Mark Becher <mbecher@stud.hs-offenburg.de>
2024-04-28 13:29:54 +0200

    test xml file added as well as xsd file for validation, method for validation integrated in Flask_Server.py

--- Flask_Server.py
+++ Flask_Server.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request, jsonify
+from lxml import etree
+
+def validate_client_data(xml : str, xsd : str) -> bool:
+    """
+    Diese Methode soll die empfange Nachricht (XML Datei) gegen die vorhandene DTD validieren.
+    """
+
+    xsdscema_source = etree.parse(xsd)
+    xsdscema = etree.XMLSchema(xsdscema_source)
+
+    xml_source = etree.parse(xml)
+
+    return xsdscema.validate(xml_source)
+
+
 
 app = Flask(__name__)
 
 @app.route('/api/data', methods=['POST'])
 def get_data():
-    data = request.json
+    data = request.json() #darf nicht in json format sein, muss geandert werden.
+
+    #empfangene Daten validieren
+    if not validate_client_data(data, 'clientData.xsd'):
+        return False
+
     #Daten verarbeiten
     response_data = {'message': 'received data', 'received_data': data}
     print(jsonify(response_data))
